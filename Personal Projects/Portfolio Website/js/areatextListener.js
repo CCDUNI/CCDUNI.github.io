23ffa3af4ad83daf62a2ede04ad1524d3e8d2c25
CCDUNI <62255963+CCDUNI@users.noreply.github.com>
2025-07-10 17:41:29 +1000

    - Added sound effects for each key press - Added some cosmetic effects to the html elements - Added input functionality for grabbing typed commands from the textarea

--- Personal Projects/Portfolio Website/js/areatextListener.js
+++ Personal Projects/Portfolio Website/js/areatextListener.js
@@ -1,12 +1,79 @@
+/*/
+Changed sound to be preloaded in the future - though it seems like the key press sound effects
+where delayed as a result of browser issues; firefox has audio bugs, safari plays them with little delay though
+some of the key presses can lag.
+
+Issue #1: When typing fast, the whole program boggs down - maybe limit the play speed of the sound.
+ */
+
+
 const outputBox = document.getElementById('entryBox');
 let currentLineIndex = 0;
-
-outputBox.value = 'currDirectory> ';
-currentLineIndex = outputBox.value.length;
+let keyPress_Future = preloadKeyPress();
+outputBox.value = "directory> ".toUpperCase();
+currentLineIndex = outputBox.value.length-1;
 outputBox.selectionStart = outputBox.selectionEnd = currentLineIndex;
+
+async function playEntryAnimation(){
+    const entryBox = document.getElementById("entryBox");
+    const animation = document.getElementById("animatedText");
+
+    let enteredCommand = outputBox.value.slice(0,-1);
+    console.log(enteredCommand);
+    outputBox.value += '\ncurrDirectory> '.toUpperCase();
+    currentLineIndex = outputBox.value.length-1;
+    outputBox.selectionStart = outputBox.selectionEnd = currentLineIndex;
+
+    animation.textContent = enteredCommand;
+    animation.style.display = 'block';
+    entryBox.style.display = 'none';
+
+
+
+}
+
+async function preloadKeyPress(){
+    const max = 33; //the number of keypress sound files
+    const randomIndex = Math.floor(Math.random() * max) + 1; //+1 because the files are indexed starting from 1
+    const soundPath = `Personal Projects/Portfolio Website/Sounds/Key_Presses/Keypress_${randomIndex}.wav`
+
+    const audio = new Audio(soundPath);
+    audio.preload = 'auto';
+
+    await new Promise((resolve, reject) => {
+        audio.addEventListener('canplaythrough', () => {
+            console.log('Preloaded: ${soundPath}!');
+            resolve();
+        }, {once: true});
+
+        audio.addEventListener('error', (e) => {
+            console.error(e);
+            reject(new Error(`Error: ${e}`));
+        })
+        audio.load();
+    })
+    keyPress_Future = audio;
+}
+
+function playKeyPress(){
+    if (keyPress_Future){
+        keyPress_Future.pause();
+        keyPress_Future.currentTime = 0;
+        keyPress_Future.play().catch (error => console.log(error));
+        preloadKeyPress();
+    }
+    else{
+        Console.warn('No sound loaded');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () =>{
+    preloadKeyPress();
+});
 
 document.addEventListener('keydown', function(event){
     const pressedKey = event.key;
+    playKeyPress();
     //change switch statements to objects/maps for later expansion
     switch(pressedKey){
         case "Backspace":
@@ -18,16 +85,15 @@ document.addEventListener('keydown', function(event){
             }
             break;
         case "Enter":
-            outputBox.value += '\ncurrDirectory> ';
-            currentLineIndex = outputBox.value.length;
-            outputBox.selectionStart = outputBox.selectionEnd = currentLineIndex;
+            playEntryAnimation();
             break;
         default:
-            outputBox.value += pressedKey;
+            outputBox.value += pressedKey.toUpperCase();
             outputBox.selectionEnd++;
     }
 
     outputBox.style.height = 'auto';
     outputBox.style.height = outputBox.scrollHeight +'px';
-
 });
+
+
